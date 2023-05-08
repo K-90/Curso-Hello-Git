0d8c1441e28fe0d118c39fd86a0bfda3199e1326
kike <enriquecastroromero62@gmail.com>
2023-05-08 12:07:19 +0200

    v2.1

--- Prueba.py
+++ Prueba.py
@@ -1,9 +1,21 @@
 # Pruebas con Git y GitHub
 
-class git():
-    add = 0
-    commit = 1
-    push = 2
-    merge = 3
+# Comando principales:
+# git --version -> versión actual de git
+# git init -> inicializar un repositorio vacio
+# git add -> añadir un archivo 
+# git commit -> comitear(comentar) un archivo 
+# git status -> estado de los commit 
+# git branch -> crear una rama
+# git branch --list -> ver las ramas de un repositorio
+# git branch -d <rama> -> borrar una rama
+# git checkout <rama> -> trabajar en una rama
+# git checkout <rama> -> crear una rama y trabajar en ella
+# git log -> estado actual del repositorio
+# git log --graph --all --one line -> visualizar los commit en una linea
+# git checkout <commit> -> moverse entre diferentes commit
 
-print("Hello Git")
+# git alias -> crear un alias 
+# git merge -> 
+# git push -> 
+# git reset-hard ->
